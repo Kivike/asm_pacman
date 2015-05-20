1b39ade59d1275b70e17308adffc812c1a32376d
Kivike <r_rajala@yahoo.com>
2015-05-20 21:15:58 +0300

    KeyboardInterrupt ja oldintoffin ja oldintsegin tallennus ja palautus

--- pacman.asm
+++ pacman.asm
@@ -1,1 +1,18 @@
 ; Pacman
+BITS 16
+
+segment stack stack
+			resb 100h
+			
+segment data data
+
+segment code code
+
+..start:
+	mov ah,00h			;Set Video Mode
+	mov al,13h			;320x200 Graphics, 256 colors, 1 page
+	int 10h				;Video reference interrupt 
+	
+
+
+.end
