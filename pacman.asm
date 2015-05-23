f6b22cba2747daac44ce7cc4e6052461c853b615
Urauth <pee-94@hotmail.com>
2015-05-23 18:22:05 +0300

    Reformated Code

--- pacman.asm
+++ pacman.asm
@@ -69,180 +69,177 @@ KeybInt:
         mov     word [pressesc], 1
 	    jmp    .kbread
 .cplus:
-	    cmp 	al, 04Eh	; 4E is the 'make code' for keypad plus
+	    cmp 	al, 04Eh		; 4E is the 'make code' for keypad plus
 	    jne 	.cminus
 	    ;something here later
 	    jmp     .kbread
 .cminus:
-	    cmp 	al, 04Ah	; 4A is the 'make code' for keypad minus
+	    cmp 	al, 04Ah		; 4A is the 'make code' for keypad minus
 	    jne	.kbread
 	    ;something here later
 
 .kbread:
-        in      al,61h        ; Send acknowledgment without
-        or      al,10000000b  ; modifying the other bits.
-        out     61h,al        ;                            
-        and     al,01111111b  ;                            
-        out     61h,al        ;                            
-        mov     al,20h        ; Send End-of-Interrupt signal 
-        out     20h,al        ;                              
-        sti                   ; Enable interrupts again
+        in      al,61h          ; Send acknowledgment without
+        or      al,10000000b    ; modifying the other bits.
+        out     61h,al                                    
+        and     al,01111111b                           
+        out     61h,al                                   
+        mov     al,20h          ; Send End-of-Interrupt signal 
+        out     20h,al          ;                              
+        sti                     ; Enable interrupts again
         pop     ax		
-   	    pop 	ds       				; Regain the ds,ax from stack
+   	    pop 	ds       		; Regain the ds,ax from stack
         iret	                ; Return from interrupt
 		
 
 
 copybackground:
-	push 
-	pusha
-	;Pointers
-	mov word si, 0
-	mov word di, 0
-	mov cx,64000
+		push 
+		pusha
+		;Pointers
+		mov word si, 0
+		mov word di, 0
+		mov cx,64000
 
-	;Segment registers to correct locations
-	mov ax,memscreen						; Destination segment
-	mov es,ax
-	mov ax,background						; Source segment
-	mov ds,ax
-	
-	;REPEAT COPY!
-	rep movsb										; Move byte at address ds:si to address es:di
-	popa
-	pop ds
-	ret
+		;Segment registers to correct locations
+		mov ax,memscreen		; Destination segment
+		mov es,ax
+		mov ax,background		; Source segment
+		mov ds,ax
+		
+		;REPEAT COPY!
+		rep movsb				; Move byte at address ds:si to address es:di
+		popa
+		pop ds
+		ret
 
 
 	
 drawPacman:
 
 copymemscreen:
-	push 
-	pusha
-	;Pointers
-	mov word si, 0
-	mov word di, 0
-	mov cx,64000
+		push 
+		pusha
+		;Pointers
+		mov word si, 0
+		mov word di, 0
+		mov cx,64000
 
-	;Segment registers to correct locations
-	mov ax,videobase					; Destination segment
-	mov es,ax
-	mov ax,memscreen						; Source segment
-	mov ds,ax
-	
-	;REPEAT COPY!
-	rep movsb										; Move byte at address ds:si to address es:di
-	popa
-	pop ds
-	ret
+		;Segment registers to correct locations
+		mov ax,videobase		; Destination segment
+		mov es,ax
+		mov ax,memscreen		; Source segment
+		mov ds,ax
+		
+		;REPEAT COPY!
+		rep movsb				; Move byte at address ds:si to address es:di
+		popa
+		pop ds
+		ret
 
 draw:
-	call copybackground
-	call drawPacman
-	call copymemscreen
+		call copybackground
+		call drawPacman
+		call copymemscreen
 	
 ..start:
 		
-	mov ax, mydata
-	mov ds, ax
-	mov ax, mystack
-	mov ss, ax
-	mov sp, stacktop
-	
-	mov ah,35h
-	mov al,9
-	int 21h											;Vanhat arvot talteen -> es:bx
-	mov [oldintseg],es
-	mov [oldintoff],bx
-	
-	push ds
-	mov dx,KeybInt							; Oman keyboard interruptin alkuaddress
-	mov bx,mycode								
-	mov ds,bx
-	mov al,9
-	mov ah,25h
-	int 21h											;Asetetaan oma keyboard interrupt
-	pop ds
-	
-	mov ah,0fh
-	int 10h											;Haetaan nykyinen videomode
-	mov [oldvideomode],ah
-	
-	mov ah,00h
-	mov al,13h
-	int 10h											;Asetetaan uusi videomode
+		mov ax, mydata
+		mov ds, ax
+		mov ax, mystack
+		mov ss, ax
+		mov sp, stacktop
+		
+		mov ah,35h
+		mov al,9
+		int 21h					;Vanhat arvot talteen -> es:bx
+		mov [oldintseg],es
+		mov [oldintoff],bx
+		
+		push ds
+		mov dx,KeybInt			;Oman keyboard interruptin alkuaddress
+		mov bx,mycode								
+		mov ds,bx
+		mov al,9
+		mov ah,25h
+		int 21h					;Asetetaan oma keyboard interrupt
+		pop ds
+		
+		mov ah,0fh
+		int 10h					;Haetaan nykyinen videomode
+		mov [oldvideomode],ah
+		
+		mov ah,00h
+		mov al,13h
+		int 10h					;Asetetaan uusi videomode
 	
 	
 .mainloop:
 
-	mov ax,videobase
-	mov es,ax										;move video memory address to ESC
-	mov di,0										;move the desired offset address to DI
-	mov byte[es:di],blue				;move the constant 'blue' to the video memroy at offset DI
-	inc di											;inc offset
-	mov byte[es:di],white				;paint another pixel
-	
-	cmp word [pressesc],1
-	jne .mainloop
+		mov ax,videobase
+		mov es,ax				;move video memory address to ESC
+		mov di,0				;move the desired offset address to DI
+		mov byte[es:di],blue	;move the constant 'blue' to the video memroy at offset DI
+		inc di					;inc offset
+		mov byte[es:di],white	;paint another pixel
+		
+		call takeInput			;Take input
+
+		cmp word [pressesc],1
+		jne .mainloop
 
 takeInput:
  
-	mov ah, 9                    ;DOS: print string
-	int 21h
+		mov ah, 9               ;DOS: print string
+		int 21h
  
-	mov	ah, 1					;DOS: get character
- 	int	21h
- 	or	al, 20h					;to lowercase
+		mov	ah, 1				;DOS: get character
+ 		int	21h
+ 		or	al, 20h				;to lowercase
  
- ; Check keypresses and change the right text
-	cmp al, 'w'					
-		je Up					
+ 		; Check keypresses and call the subroutine
+		cmp al, 'w'					
+			je Up					
 
- 	cmp	al, 'a'					
-		je Left					
-	
- 	cmp	al, 's'					
-		je Down
-	
-	cmp al, 'd'					
-		je Right		
+	 	cmp	al, 'a'					
+			je Left					
+		
+	 	cmp	al, 's'					
+			je Down
+		
+		cmp al, 'd'					
+			je Right		
 
- 	jnz	takeInput
+	 	ret
  
  
  Up: 
-	 mov dx, up
-	 je  takeInput
+		je  takeInput
  
  Left:
-	 mov dx, left
-	 je  takeInput
+		je  takeInput
  
  Down:
-	 mov dx, down
-	 je  takeInput
+		je  takeInput
 	 
  Right:
-	 mov dx, right
-	 je  takeInput
+		je  takeInput
 	
 .dosexit:
-	mov word dx, [oldintoff]
-	mov word bx, [oldintseg]
-	mov ds,bx
-	mov al,9
-	mov ah,25h
-	int 21h											;Vanhat arvot takas
-	
-	mov word dx, [oldvideomode]
-	mov ah,00h
-	mov al,13h
-	int 10h											;Vanha videomode takas
-	
-	mov	al, 0
-	mov     ah, 4ch
-	int     21h
+		mov word dx, [oldintoff]
+		mov word bx, [oldintseg]
+		mov ds,bx
+		mov al,9
+		mov ah,25h
+		int 21h											;Vanhat arvot takas
+		
+		mov word dx, [oldvideomode]
+		mov ah,00h
+		mov al,13h
+		int 10h											;Vanha videomode takas
+		
+		mov	al, 0
+		mov ah, 4ch
+		int 21h
 
 .end
->>>>>>> origin/roopenbranch
